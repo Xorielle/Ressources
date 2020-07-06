0ac0f2f8ec4ea4116a86bcbaa630196222c4f3c9
Xorielle <anne.delet@protonmail.com>
2020-07-06 10:48:34 +0200

    Does not work ! Issue with the list of variables...

--- BDD Test/addRow.py
+++ BDD Test/addRow.py
@@ -29,6 +29,7 @@ for row in description:
     columns.append(row[0])
     type_column.append(row[1])
 
+sizeTable = len(columns)
 
 # Get the information of the row we want to add
 row_input = []
@@ -38,18 +39,34 @@ for column in columns:
 
 
 # Verify the row is correct before committing the data base
-print('Est-ce que les données à ajouter à la table ' + usedTable + ' sont correctes ? \n')
-for column in range (0, len(columns)):
-    print(columns[column], row_input[column], "\n")
+for column in range (0, sizeTable):
+    print("\n", columns[column], row_input[column])
 
+print("\n", 'Est-ce que les données ci-dessus à ajouter à la table ' + usedTable + ' sont correctes ?')
 answer = input ('Oui [O] ou Non [N] ? ')
-assert answer == 'O' or answer == 'N', 'Entrer O ou N en majuscule'
+assert answer == 'O' or answer == 'N', 'Entrer O ou N en majuscule' # Trouver comment ne pas revenir au début du programme...
 
 #if answer == 'O':
 #    sql = """INSERT INTO %s VALUES (""" % usedTable
 #    for nb in range (O, len(columns)-1):
 #        sql."%s", "%s"
+#   
+sql = ["""INSERT INTO %s VALUES ("""]
+variables = ["%" + " (" + usedTable]
+for nb in range (0,sizeTable-1):
+    sql.append("%s, ")
+    variables.append(", "+row_input[nb])
+sql.append("%s); ")
+variables.append(", "+row_input[sizeTable-1] + ")")
 
+print(sql)
+print(variables)
+variables = "".join(variables)
+print(variables)
+sql = ["".join(sql)]
+sql.append(variables)
+print(sql)
+print("".join(sql))
 
 
 db.close()
