cc9e05fa35ddeb6e3d1e30701c44c6403f8a9cb7
Xorielle <anne.delet@protonmail.com>
2020-07-06 15:26:26 +0200

    Adding row in M or in P is possible

--- BDD Test/addRow.py
+++ BDD Test/addRow.py
@@ -30,7 +30,6 @@ for row in description:
     type_column.append(row[1])
 
 sizeTable = len(columns)
-print(type_column)
 
 
 # Get the information of the row we want to add
@@ -44,34 +43,27 @@ for column in columns:
 for column in range (0, sizeTable):
     print("\n", columns[column], row_input[column])
 
-print("\n", 'Est-ce que les données ci-dessus à ajouter à la table ' + usedTable + ' sont correctes ?')
+print('\nEst-ce que les données ci-dessus à ajouter à la table ' + usedTable + ' sont correctes ?')
 answer = input ('Oui [O] ou Non [N] ? ')
 assert answer == 'O' or answer == 'N', 'Entrer O ou N en majuscule' # Trouver comment ne pas revenir au début du programme...
 # + let the possibility to go back and modify the inputs without writing all again
 
 
 # Prepare to add in db with correct type
-#if answer == 'O':
-#    row_input_typed = ()
-#    for nb in range(0,sizeTable-1):
-#        row_input_typed.append(row_input[nb])
-#    row_input_typed.append(row_input[sizeTable-1])
-#
-#else:
-#    print("mauvaise entrée")
+if answer == 'O':
+    sql_command = ["(%s"]
+    for nb in range(1, sizeTable):
+        sql_command.append(", %s")
+    sql_command.append(");")
 
- 
-print("""INSERT INTO """ + usedTable + """ VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);""" % tuple(row_input))
+else:
+    print("mauvaise entrée")
 
 
+
+# Add the row in the db if there is no SQL issue
 try:
-    cursor.execute("SHOW WARNINGS;")
-    warnings = cursor.fetchall()
-    print("warnings 1: ", warnings)
-    cursor.execute("""INSERT INTO """ + usedTable + """ VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);""", tuple(row_input))
-    cursor.execute("SHOW WARNINGS;")
-    warnings = cursor.fetchall()
-    print("warnings 2: ", warnings)
+    cursor.execute("INSERT INTO %s VALUES " % usedTable + "".join(sql_command), tuple(row_input))
     print("Ligne ajoutée")
     db.commit()
     print("BDD mise à jour avec succès")
