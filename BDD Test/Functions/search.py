47e7a3b36648405373b109e7662b6ac12cec4731
Xorielle <anne.delet@protonmail.com>
2020-07-08 16:56:06 +0200

    Work in progress : search and search functions

--- BDD Test/Functions/search.py
+++ BDD Test/Functions/search.py
@@ -5,3 +5,48 @@
 import pymysql
 
 
+def chooseTable():
+    """Choose wether we are searching in Materiaux or Pieces
+    Return usedTable"""
+    table = input("Voulez-vous effectuer une recherche parmi les matériaux [M] ou les pièces [P] ? ")
+    
+    if table == "M": 
+        return("Materiaux")
+    
+    if (table == "P"):
+        return("Pieces")
+    
+    else:
+        return(chooseTable())
+
+
+def getColumns(usedTable, cursor):
+    """Create the list of all columns/criterias in the table
+    Return columns"""
+    columns = []
+    cursor.execute("DESCRIBE %s;" % usedTable)
+    description = cursor.fetchall()
+    
+    for row in description:
+        columns.append(row[0])
+    
+    return(columns)
+
+
+def getColumnToSearch(columns):
+    """Ask in which column (that means wich criteria) the user wants to search
+    Return searched_column"""
+    print("Dans quelle colonne souhaitez-vous effectuer une recherche ? Tapez Entrée jusqu'à arriver à la colonne souhaitée, puis tapez n'importe quelle touche pour cette colonne-ci.")
+    answer = ""
+    nb = 0
+
+    while answer == "":
+        answer = input(columns[nb] + " ? ")
+        nb += 1
+    
+    return(columns[nb-1])
+
+
+
+def getSearchTerm(usedTable):
+    """TODO Ask which term is searched in the column"""
--- BDD Test/search.py
+++ BDD Test/search.py
@@ -7,3 +7,25 @@ import Functions.search as fct
 import Functions.connectionDb as conn
 
 
+# Initialize users parameters and connection to DB
+user_name, date = conn.parameters()
+db, cursor = conn.connectionToDb(user_name)
+
+
+answer_abort = "O"
+
+# TODO only simple search for the moment
+
+while answer_abort == "O":
+    # Ask about what is being searched
+    usedTable = fct.chooseTable()
+    columns = fct.getColumns(usedTable, cursor)
+    column = fct.getColumnToSearch(columns)
+    print(column)
+    # Re-modelling to have a displayable information
+    # Print data
+    answer_abort = input("Souhaitez-vous continuer à consulter la base de données ? [O] pour continuer, toute autre touche pour quitter ")
+    
+print("Sortie de la BDD")
+
+db.close()
