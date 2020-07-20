278e47693385171d549d7eb358330cd1ae50428d
Xorielle <anne.delet@protonmail.com>
2020-07-20 16:14:20 +0200

    Completed program with modify and delete functions.

--- Test/merge.py
+++ Test/merge.py
@@ -23,8 +23,14 @@ if answer == "O":
     new_values, old_values, entered_values = fct.getNewValues(row1, row2, columns, sizeTable)
     values = fct.buildValues(new_values, entered_values, sizeTable)
     request = fct.buildSQLrequest(values, columns, sizeTable, usedTable, id1, date, user_name)
-
+    modification = modify.executeModification(request, cursor, db)
+    deletion = delete.delete(id2, usedTable, cursor, db)
     
+    if modification and deletion:
+        print("La fusion a été effectuée correctement.")
+    else:
+        print("Il y a eu une erreur au cours de la fusion") # db.commit is executed after both functions. This may cause LOSS OF DATA.
+
 else:
     print("Revenez avec les IDs !")
 
