4edb386c11701114d1b95d41c786adb914bb8f12
Xorielle <anne.delet@protonmail.com>
2020-07-21 11:43:22 +0200

    Date written in file's name.

--- Test/Functions/backup.py
+++ Test/Functions/backup.py
@@ -2,5 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import pymysql
+import datetime
 
 
+def parameters():
+    """Fetch the username, password and the date to backup the system
+    Return (user_name, password, date)"""
+    user_name = input('Qui utilise la BDD (super-utilisateur) ? ')
+    password = input('Mot de passe : ')
+    full_date = datetime.datetime.now()
+    date = "%s-%s-%s" % (full_date.strftime("%Y"), full_date.strftime("%m"), full_date.strftime("%d"))
+    print("Date du jour : ", date)
+    
+    return(user_name, password, date)
--- Test/backup.py
+++ Test/backup.py
@@ -2,10 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import pymysql
-import Functions.connectionDb as conn
 import subprocess
+import Functions.connectionDb as conn
+import Functions.backup as fct
 
+# Get the information to create the name of the file
+user_name, date = conn.parameters()
 
-subprocess.run("mysqldump -u xorielle --opt TestDB > testPython.sql", shell=True)
+subprocess.run("mysqldump -u %s --opt TestDB > TestDB_%s.sql" % (user_name, date), shell=True)
+print("La sauvegarde de la base de données a bien été effectuée.")
 # No idea why I needed to add shell=True
 # On windows : add Path (with command ? execute the right command to set the path before ?)
+
+# TODO Restaurer la dernière sauvegarde
+# TODO Restaurer une sauvegarde plus ancienne (entrer la date manuellement)
+
+
+# Get the right information to have the name of the file
+#user_name, password, date = fct.parameters()
