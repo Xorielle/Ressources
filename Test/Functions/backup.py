5d52bf598d85d7dd3a31cda7482aca43b0f47368
Xorielle <anne.delet@protonmail.com>
2020-07-21 12:34:44 +0200

    Fix #28
    Can backup and restore the DB using the date.

--- Test/Functions/backup.py
+++ Test/Functions/backup.py
@@ -7,7 +7,7 @@ import datetime
 
 def askBackup():
     """Return 'O' or 'N'."""
-    print("Attention, effectuer une sauvegarde effacera toute sauvegarde précédemment effectuée aujourd'hui.")
+    print("\nAttention, effectuer une sauvegarde effacera toute sauvegarde précédemment effectuée aujourd'hui.")
     answer = input("Souhaitez-vous effectuer une nouvelle sauvegarde de la base de données ? [O/N] ")
 
     if (answer == "O") or (answer == "N"):
@@ -19,7 +19,7 @@ def askBackup():
 
 def askRestore():
     """Return 'O' or 'N'"""
-    print("Attention, restaurer une sauvegarde effacera toute modification effectuée depuis la sauvegarde.")
+    print("\nAttention, restaurer une sauvegarde effacera toute modification effectuée depuis la sauvegarde.")
     answer = input("Souhaitez-vous restaurer une sauvegarde de la base de données ? [O/N] ")
 
     if (answer == "O") or (answer == "N"):
@@ -32,7 +32,7 @@ def askRestore():
 def dateBackup(date):
     """Returns the date of the backup to restore"""
     print("Date du jour : ", date)
-    answer = input("Souhaitez-vous utiliser la date du jour ? [O/N]")
+    answer = input("Souhaitez-vous utiliser la date du jour ? [O/N] ")
 
     if answer == "O":
         return(date)
@@ -44,3 +44,6 @@ def dateBackup(date):
 
     else:
         return(dateBackup(date))
+
+
+    
--- Test/backup.py
+++ Test/backup.py
@@ -14,11 +14,18 @@ answerBackup = fct.askBackup()
 # Create the path to the folder where the backups are stored
 
 if answerBackup == "O":
-    subprocess.run("mysqldump -u %s --opt TestDB > TestDB_%s.sql" % (user_name, date), shell=True)
-    print("La sauvegarde de la base de données a bien été effectuée.")
-    print("Sortie du programme.")
-# No idea why I needed to add shell=True
+
 # On windows : add Path (with command ? execute the right command to set the path before ?)
+    
+    try:
+        subprocess.run("mysqldump -u %s --opt TestDB > TestDB_%s.sql" % (user_name, date), shell=True)
+        # No idea why I needed to add shell=True
+        print("La sauvegarde de la base de données a bien été effectuée.")
+    
+    except:
+        print("La sauvegarde a échoué.")    
+    
+    print("Sortie du programme.")
 
 else: # Considering nobody is willing to save the database and then restore another version. Should be change ?
     # Also knowing the program can just be run two times if someone wants it.
@@ -26,6 +33,16 @@ else: # Considering nobody is willing to save the database and then restore anot
 
     if answerRestore == "O":
         dateBackup = fct.dateBackup(date)
+        
+        try:
+            subprocess.run("mysql -u %s TestDB < TestDB_%s.sql" % (user_name, dateBackup), shell=True)
+            # To restore, the DB should already be created. Create the DB manually if you are importing it on a new device.
+            print("Les données en date du %s ont bien été restaurées." % dateBackup)
+        
+        except:
+            print("Une erreur est survenue lors de la restauration des données.")    
+
+        print("Sortie du programme.")
 
     else:
         print("Sortie du programme sans aucune action.")
