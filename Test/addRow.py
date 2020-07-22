c66b04020b2a2238c55b6e1b2f053f28676ae72c
Xorielle <anne.delet@protonmail.com>
2020-07-22 18:34:14 +0200

    Replace in the print the names of the columns
    The name is now more readable.

--- Test/addRow.py
+++ Test/addRow.py
@@ -19,13 +19,14 @@ while answer_abort == "O":
 	# Ask about what will be added
 	usedTable = fct.chooseTable()
 	columns, type_columns, sizeTable, default_columns = fct.getTableStructure(usedTable, cursor)
-	raw_row_input = fct.getRowInformation(usedTable, date, user_name, sizeTable, columns, cursor)
+	namesColumns = conn.getNamesOfColumns(usedTable, cursor)
+	raw_row_input = fct.getRowInformation(usedTable, date, user_name, sizeTable, namesColumns, cursor)
 
 	answer_modification = "M"
 
 	while answer_modification == "M":
 		# Re-modelling to have the right information
-		row_input = fct.verifyRowSyntaxes(raw_row_input, sizeTable, columns, default_columns)
+		row_input = fct.verifyRowSyntaxes(raw_row_input, sizeTable, namesColumns, default_columns)
 		answer_verification = fct.userConfirmation(usedTable)
 
 		if answer_verification == 'O':
@@ -39,7 +40,7 @@ while answer_abort == "O":
 			answer_modification = input("Voulez-vous modifier vos entrées [M] ou annuler toute saisie [toute autre touche] ? ")
 			
 			if answer_modification == "M":
-				raw_row_input = fct.modifyRowInformation(row_input, sizeTable, columns)
+				raw_row_input = fct.modifyRowInformation(row_input, sizeTable, nameColumns)
 
 	answer_abort = input("Souhaitez-vous continuer à compléter la base de données ? [O] pour continuer, toute autre touche pour quitter ")
 
