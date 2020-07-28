b89c376c8637dfa812101a9b4714abaf3998490a
Xorielle <anne.delet@protonmail.com>
2020-07-28 12:03:27 +0200

    Units are now printed in search

--- search.py
+++ search.py
@@ -22,46 +22,45 @@ while answer_abort == "O":
     #Ask about the type of research and get the structure of the table
     search = fct.typeOfSearch()
     usedTable = fct.chooseTable()
-    namesColumns = conn.getNamesOfColumns(usedTable, cursor)
+    namesColumns, controlled, units = conn.getNamesOfColumns(usedTable, cursor)
     columns, type_columns, sizeTable = fct.getTableStructure(usedTable, cursor)
 
     if search == "S":
 
         # Ask about what is being searched
-        nameColumn, column, type_column = fct.getColumnToSearch(namesColumns, columns, type_columns)
+        nameColumn, column, type_column, unit = fct.getColumnToSearch(namesColumns, units, columns, type_columns)
         searched_one, searched_two = fct.getSearchCriteria(usedTable, column, type_column, cursor)
         
         # Re-modelling to have the right sql request
-        selected_columns, selected_names = fct.selectColumnsToPrint(usedTable, sizeTable, namesColumns, columns)
+        selected_columns, selected_names, selected_units = fct.selectColumnsToPrint(usedTable, sizeTable, namesColumns, units, columns)
         sql, sizeRequest = fct.prepareSQLRequestSimple(searched_one, searched_two, usedTable, selected_columns, column, type_column)
 
     elif search == "A":
 
         # Ask about what is being searched
-        s_names, s_columns, s_type_columns, sizeRequest = fct.getColumnsToSearch(namesColumns, columns, type_columns, sizeTable)
-        searched = fct.getSearchCriterias(usedTable, s_names, s_columns, s_type_columns, sizeRequest, cursor)
+        s_names, s_columns, s_type_columns, s_units, sizeRequest = fct.getColumnsToSearch(namesColumns, units, columns, type_columns, sizeTable)
+        searched = fct.getSearchCriterias(usedTable, s_names, s_columns, s_type_columns, s_units, sizeRequest, cursor)
 
         # Finalizing the sql request
-        selected_columns, selected_names = fct.selectColumnsToPrint(usedTable, sizeTable, namesColumns, columns)
+        selected_columns, selected_names, selected_units = fct.selectColumnsToPrint(usedTable, sizeTable, namesColumns, units, columns)
         sql, sizeRequest = fct.prepareSQLRequestAdvanced(usedTable, selected_columns, searched)
 
 
     # Print data
     try:
         results, description, request = fct.searchDb(sql, selected_columns, cursor)
-        print("La recherche a abouti")
-        truncated, titleHead = fct.printResults(results, description, selected_names, sizeRequest, request)
-        
+        print("La recherche a abouti.")
+        truncated, titleHead, titleUnit = fct.printResults(results, description, selected_names, selected_units, sizeRequest, request)
+
         if truncated != []:
             answer = fct.wantToPrintTruncated()
             while answer != "N":
                 if answer == "O":
-                    fct.printTruncated(truncated, titleHead, results, sizeRequest)
+                    fct.printTruncated(truncated, titleHead, titleUnit, results, sizeRequest)
                     answer = "N"
-    
+
                 else:
                     answer = fct.wantToPrintTruncated()
-
     except:
         print("Votre requête n'a pas pu aboutir.")
         
