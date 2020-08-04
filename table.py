003777b5ba8ce9ae58036070265a6368c222d962
Xorielle <anne.delet@protonmail.com>
2020-08-04 16:10:46 +0200

    Add file to extract the datas in Latex table format

---
+++ table.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import pymysql
+import Functions.connectionDb as conn
+
+
+def getNamesOfColumns(usedTable, cursor):
+    """Get the equivalent names of the columns, to have something more readable for the user.
+    Return also a "True" or "False" list to know if the column has to get the controlled words or is free of constraints."""
+    namesColumns = []
+    controlled = []
+    units = []
+    categories = []
+    
+    cursor.execute("SELECT * FROM Name%s;" % usedTable)
+    names = cursor.fetchone()
+
+    for name in names:
+        namesColumns.append(name)
+
+    constraints = cursor.fetchone()
+    for constraint in constraints:
+        controlled.append(constraint)
+
+    unitsTable = cursor.fetchone()
+    for unit in unitsTable:
+        units.append(unit)
+
+    categoriesTable = cursor.fetchone()
+    for category in categoriesTable:
+        categories.append(category)
+
+    return(namesColumns, controlled, units, categories)
+
+
+def getTableStructure(usedTable, cursor):
+    """Return the title of the columns, the type of data and the number of columns"""
+    cursor.execute('DESCRIBE %s;' % usedTable)
+    description = cursor.fetchall()
+    columns = []
+    type_columns = []
+    default_columns = []
+    null = []
+
+    for row in description:
+        columns.append(row[0])
+        type_columns.append(row[1])
+        null.append(row[2])
+        default_columns.append(row[4])
+    sizeTable = len(columns)
+
+    return(columns, type_columns, sizeTable, default_columns, null)
+
+
+def createRows(sizeTable, columns, namesColumns, type_columns, default_columns, null, controlled, units, categories):
+    f = open("table.txt", "w")
+    f.write("\\begin{tabular}{|c|c|c|c|c|c|c|c|}\n\\hline\nTitre & Nom affiché & Type & Unité & Catégorie d'affichage & Limité ? & Valeur par défaut & Peut être vide ?")
+    for i in range(sizeTable):
+        unit = units[i]
+        default_column = default_columns[i]
+        if unit == None:
+            unit = ""
+        if default_column == None:
+            default_column = ""
+        f.write("\n\\hline\n%s & %s & %s & %s & %s & %s & %s & %s" % (columns[i], namesColumns[i], type_columns[i], unit, categories[i], controlled[i], default_column, null[i]))
+    f.write("\n\hline\n\\end{tabular}")
+    f.close()
+
+
+db, cursor = conn.connectionToDb(username = 'xorielle')
+columns, type_columns, sizeTable, default_columns, null = getTableStructure("Materiaux", cursor)
+namesColumns, controlled, units, categories = getNamesOfColumns("Materiaux", cursor)
+createRows(sizeTable, columns, namesColumns, type_columns, default_columns, null, controlled, units, categories)
+
